96743ff77d9b484cc689ce9dcbcf870f95f78470
YourGitHubUsername <your.email@example.com>
2023-12-11 04:24:23 +0530

    commit 6

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -36,9 +36,9 @@ const Contact = () => {
         </div>
         <div className="c-right">
         <form ref={form} onSubmit={sendEmail}>
-          <input type="text" name="user_name" className="user"  placeholder="Name"/>
-          <input type="email" name="user_email" className="user" placeholder="Email"/>
-          <textarea name="message" className="user" placeholder="Message"/>
+          <textarea type="text" name="user_name" className="user"  placeholder="Name"/>
+          <textarea type="email" name="user_email" className="user" placeholder="Email"/>
+          <textarea name="message" className="user" placeholder="Message & Email"/>
           <input type="submit" value="Send" className="button"/>
           <span>{done && "Thanks for Contacting me"}</span>
           
